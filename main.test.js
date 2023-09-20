cdc1e1323ce37a57e5d7df541e7fa0f64dffb7c8
stephen <sdelius.prsvr@gmail.com>
2023-09-20 10:25:17 -0500

    main

---
+++ main.test.js
@@ -0,0 +1,134 @@
+/**********************************
+ * YOUR CODE GOES HERE THIS TIME! *
+ **********************************/
+let x = 3
+let y = 10
+let z = 4
+let greeting = 'hello'
+let meanName = 'Stinky Buttface'
+let callStatus = 'ringing'
+
+function double() {
+  x = x *2
+}
+
+function triple() {
+  y = y *3
+}
+
+function square() {
+  z = z * z
+}
+
+function beMean() {
+  greeting = greeting + ' ' + meanName
+  console.log(greeting)
+}
+
+function changeName() {
+  meanName = 'Danger'
+}
+
+function hangUp() {
+  callStatus = 'call ended'
+}
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof x === 'undefined') {
+  x = undefined;
+}
+
+if (typeof y === 'undefined') {
+  y = undefined;
+}
+
+if (typeof z === 'undefined') {
+  z = undefined;
+}
+
+if (typeof greeting === 'undefined') {
+  greeting = undefined;
+}
+
+if (typeof meanName === 'undefined') {
+  meanName = undefined;
+}
+
+if (typeof callStatus === 'undefined') {
+  callStatus = undefined;
+}
+
+if (typeof double === 'undefined') {
+  double = undefined;
+}
+
+if (typeof triple === 'undefined') {
+  triple = undefined;
+}
+
+if (typeof square === 'undefined') {
+  square = undefined;
+}
+
+if (typeof beMean === 'undefined') {
+  beMean = undefined;
+}
+
+if (typeof changeName === 'undefined') {
+  changeName = undefined;
+}
+
+if (typeof hangUp === 'undefined') {
+  hangUp = undefined;
+}
+
+
+describe('double', () => {
+  it(`doubles x`, () => {
+    double();
+    expect(x).toEqual(6);
+  })
+});
+
+describe('triple', () => {
+  it(`triples y`, () => {
+    triple();
+    expect(y).toEqual(30);
+  })
+});
+
+describe('square', () => {
+  it(`squares the variable z`, () => {
+    square();
+    expect(z).toBe(16);
+  })
+});
+
+describe('beMean', () => {
+  it(`says hello to the person and calls them a mean name`, () => {
+    beMean();
+    expect(greeting.includes('hello') && greeting.includes(meanName)).toBe(true);
+  })
+});
+
+describe('changeName', () => {
+  it(`changes the value of meanName to 'Danger'`, () => {
+    changeName();
+    expect(meanName).toBe('Danger')
+  })
+});
+  
+describe('hangUp', () => {
+  it(`changes the value of callStatus to be 'call ended'`, () => {
+    hangUp();
+    expect(callStatus).toBe('call ended')
+  })
+});
